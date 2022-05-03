36d5796d9e94a571aee8febda38940b5acce9875
Alex Handlovits <ahandlovits@gmail.com>
2022-05-03 15:00:08 -0400

    separate API into its own file

---
+++ app.js
@@ -0,0 +1,179 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const emitter = require('./helpers/emitter');
+const mongoose = require("mongoose");
+const User = require("./db/User");
+const Sound = require("./db/Sound");
+mongoose.connect("mongodb://localhost/bloop");
+const commands = require('./commands');
+let client = null;
+
+const API = function(_client) {
+client = _client;
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+var corsOptions = {
+    origin: ['http://localhost:3000', 'https://bloopbot.netlify.app'],
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+app.use(express.static(__dirname + '/sounds'));
+
+app.post('/api/getToken', async (req, res) => {
+    if (corsOptions.origin.includes(req.headers.referer))
+    return res.send("unauthorized origin");
+
+    const params = new URLSearchParams();
+      params.append('client_id', process.env.DISCORD_CLIENT_ID);
+      params.append('client_secret', process.env.DISCORD_CLIENT_SECRET);
+      params.append('grant_type', 'authorization_code');
+      params.append('code', req.query.code);
+      params.append('redirect_uri', `${req.headers.referer}auth/redirect`);
+      // fetch the access token
+      axios
+        .post('https://discord.com/api/v8/oauth2/token', params, {
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+          }
+        }).then(result => {
+            return res.send(result.data);
+        })
+});
+
+app.post('/api/setToken', async (req, res) => {
+    try {
+        const tokenResponse = await axios.get('https://discordapp.com/api/users/@me', {
+            headers: {
+            Authorization: `Bearer ${req.body.access_token}`
+            }
+        });
+        let user = null;
+        if (tokenResponse.status === 200) {
+            user = await User.findOneAndUpdate(
+                {userId: tokenResponse.data.id}, // find existing user
+                {                                // user information to update
+                    ...req.body,
+                    userId: tokenResponse.data.id,
+                    username: tokenResponse.data.username,
+                    updatedAt: Date.now()
+                },
+                {upsert: true, new: true}, // options (upsert: create on not found, new: return updated user after transaction)
+            )
+        }
+        return res.status(tokenResponse.status).send(user);
+    } catch (e) {
+        console.log("error in /api/setToken", e);
+        return res.sendStatus(401);
+    }
+});
+
+/////////////// required authorization routes below ///////////////
+
+app.use(async (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ error: 'No credentials sent!' });
+    } 
+    const isValidUser = await validateUser(req);
+    if (!isValidUser) {
+        return res.status(403).json({ error: 'Not a valid discord user!' });
+    }
+    next();
+})
+
+app.post('/api/play', (req, res) => {
+    if (client.guilds.cache.get(req.body.guildId).channels.cache.get(req.body.channelId).members.size === 0) {
+        return res.status(405).json({ message: "Can't send sound bite to empty voice channel!" });
+    }
+    emitter.emit("api/play", req.body);
+    return res.sendStatus(200);
+})
+
+app.post('/api/servers', (req, res) => {
+    let botGuilds = client.guilds.cache.map(g => g.id);
+    let userGuilds = req.body.guilds;
+    let matchingGuilds = userGuilds.filter(g => botGuilds.includes(g));
+    return res.send(matchingGuilds);
+})
+app.get('/api/sound', (req, res) => {
+    let soundRequest = req.query.name;
+    try {
+        let soundBytes = fs.readFileSync(`./sounds/${soundRequest}.opus`);
+        return res.send(soundBytes);
+        // const filePath = resolve(`./sounds/${soundRequest}.opus`);
+        // res.sendFile(filePath)
+    } catch(e) {
+        console.log(e);
+        return res.sendStatus(404);
+    }
+
+})
+app.get('/api/sounds', async (req, res) => {
+    //let guild = req.query.server; // one day when sounds are tied to servers bot will need to get server id from query param
+    let soundData = await Sound.find({});
+    return res.send(soundData);
+})
+app.post('/api/channels', async (req, res) => {
+    let channels = client.guilds.cache.get(req.body.guildId).channels.cache
+    let guild = await client.guilds.fetch(req.body.guildId);
+    let user = await guild.members.fetch(req.body.userId);
+
+    let result = [];
+    channels.forEach(c => {
+        if(c.type === "GUILD_VOICE" && user.permissionsIn(c).has("CONNECT")) {
+            result.push({id: c.id, name: c.name})
+        }
+    })
+    return res.send(result);
+})
+
+app.post('/api/add', async (req, res) => {
+    let addResponse = await commands.add(req.body.args, null, req.body.username);
+    return res.status(addResponse[0]).send(addResponse[1]);
+})
+
+app.post('/api/setFavorite', async (req, res) => {
+    let soundId = req.body?.soundId;
+    if (!soundId) {
+        return res.status(401).send('Invalid request format, unable to set favorite');
+    }
+    let userId = JSON.parse(req.headers.authorization).id;
+    let user = await User.findOne({ userId: userId});
+    if (!user) {
+        return res.status(401).send('Can\'t find user to update, try again later');
+    }
+    let favoriteIndex = user.favorites.indexOf(soundId);
+    user.updatedAt = Date.now();
+    if (favoriteIndex === -1) {
+        user.favorites.push(soundId);
+    } else {
+        user.favorites.splice(favoriteIndex, 1);
+    }
+    user.save().then(() => {
+        return res.status(200).send({favorites: user.favorites});
+    }).catch(e => {
+        return res.status(500).send('Error saving favorites, try again later');
+    });
+})
+
+async function validateUser(req) {
+    console.log("auth", req.headers.authorization);
+    let auth = JSON.parse(req.headers.authorization);
+    let user = await User.findOne({
+        userId: auth.id, 
+        access_token: auth.access_token
+    });
+    return !!user;
+}
+
+app.listen(PORT, () => {
+    console.log(`Now listening to requests on port ${PORT}`);
+});
+}
+
+module.exports = API;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
-const axios = require('axios');
+const app = require('./app');
+const User = require("./db/User");
+const emitter = require('./helpers/emitter');
+// setup logging
 const { createLogger, format, transports } = require('winston');
 const logger = createLogger({
   level: 'info',
@@ -86,7 +89,10 @@ try {
 }
 
 client.on("ready", () => {
-  console.log("bot is online");
+    emitter.on('api/play', (body) => {
+        client.emit("api/play", body);
+    })
+    console.log("bot is online");
 });
 
 client.on("error", (e, message) => {
@@ -154,11 +160,8 @@ process.on('unhandledRejection', (error) => {
 });
 
 client.on("messageCreate", async (message) => {
-	// console.log("message = ", message);
-	// console.log("message.channel.id = ", message.channel.id);
-	// console.log("message.content = ", message.content);
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
+    // activate production maintenance mode lol (todo: make 'staging' bot)
     // if (message.member.displayName !== 'Yoduh') {
     //     message.reply("Yoduh says \"Bot is undergoing maintenance right now, SORRY\"");
     //     return;
@@ -228,15 +231,6 @@ client.on("messageCreate", async (message) => {
             case "entrance":
                 commands.entrance(args, message);
                 break;
-            // i have no idea why this doesn't work.  would be great if it did and bloop bot didn't have to deal with AudioPlayer and juggling connection subscriptions
-            // case "test":
-            //     conn = await joinVoiceChannel({
-            //         channelId: message.member.voice.channel.id,
-            //         guildId: message.guild.id,
-            //         adapterCreator: message.guild.voiceAdapterCreator
-            //     });
-            //     conn.playOpusPacket(fs.readFileSync('./sounds/bruh.opus'));
-            //     break;
             default:
                 clearTimeout(timeoutID);
                 let defPlayResult = await commands.play([command], false, message, player, queue, subscription);
@@ -268,8 +262,7 @@ client.on('messageReactionAdd', (reaction, user) => {
     editEmbed(reaction.message, emoji);
 });
 
-////////////////////// API code /////////////////////////////
-client.on("apiMessage", async (apiMessage) => {
+client.on("api/play", async (apiMessage) => {
     let message = {
         guildId: apiMessage.guildId,
         member: { voice: { channel: {id: apiMessage.channelId}}},
@@ -302,174 +295,4 @@ client.on("apiMessage", async (apiMessage) => {
     }, timeout)
 });
 
-const express = require('express');
-const cors = require('cors');
-const mongoose = require("mongoose");
-const User = require("./db/User");
-const Sound = require("./db/Sound");
-mongoose.connect("mongodb://localhost/bloop");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-
-var corsOptions = {
-    origin: ['http://localhost:3000', 'https://bloopbot.netlify.app'],
-    optionsSuccessStatus: 200
-}
-
-app.use(cors(corsOptions));
-app.use(express.static(__dirname + '/sounds'));
-
-app.post('/api/getToken', async (req, res) => {
-    if (corsOptions.origin.includes(req.headers.referer))
-    res.send("unauthorized origin");
-
-    const params = new URLSearchParams();
-      params.append('client_id', process.env.DISCORD_CLIENT_ID);
-      params.append('client_secret', process.env.DISCORD_CLIENT_SECRET);
-      params.append('grant_type', 'authorization_code');
-      params.append('code', req.query.code);
-      params.append('redirect_uri', `${req.headers.referer}auth/redirect`);
-      // fetch the access token
-      axios
-        .post('https://discord.com/api/v8/oauth2/token', params, {
-          headers: {
-            'content-type': 'application/x-www-form-urlencoded'
-          }
-        }).then(result => {
-            res.send(result.data);
-        })
-});
-
-app.post('/api/setToken', async (req, res) => {
-    try {
-        const tokenResponse = await axios.get('https://discordapp.com/api/users/@me', {
-            headers: {
-            Authorization: `Bearer ${req.body.access_token}`
-            }
-        });
-        let user = null;
-        if (tokenResponse.status === 200) {
-            user = await User.findOneAndUpdate(
-                {userId: tokenResponse.data.id}, // find existing user
-                {                                // user information to update
-                    ...req.body,
-                    userId: tokenResponse.data.id,
-                    username: tokenResponse.data.username,
-                    updatedAt: Date.now()
-                },
-                {upsert: true, new: true}, // options (upsert: create on not found, new: return updated user after transaction)
-            )
-        }
-        res.status(tokenResponse.status).send(user);
-    } catch (e) {
-        console.log("error in /api/setToken", e);
-        res.sendStatus(401);
-    }
-});
-
-/////////////// required authorization routes below ///////////////
-
-app.use(async (req, res, next) => {
-    if (!req.headers.authorization) {
-        return res.status(403).json({ error: 'No credentials sent!' });
-    } 
-    const isValidUser = await validateUser(req);
-    if (!isValidUser) {
-        return res.status(403).json({ error: 'Not a valid discord user!' });
-    }
-    next();
-})
-
-app.post('/api/play', (req, res) => {
-    if (client.guilds.cache.get(req.body.guildId).channels.cache.get(req.body.channelId).members.size === 0) {
-        res.status(405).json({ message: "Can't send sound bite to empty voice channel!" });
-        return;
-    }
-    client.emit("apiMessage", req.body);
-    res.sendStatus(200);
-})
-
-app.post('/api/servers', (req, res) => {
-    let botGuilds = client.guilds.cache.map(g => g.id);
-    let userGuilds = req.body.guilds;
-    let matchingGuilds = userGuilds.filter(g => botGuilds.includes(g));
-    res.send(matchingGuilds);
-})
-app.get('/api/sound', (req, res) => {
-    let soundRequest = req.query.name;
-    try {
-        let soundBytes = fs.readFileSync(`./sounds/${soundRequest}.opus`);
-        res.send(soundBytes);
-        // const filePath = resolve(`./sounds/${soundRequest}.opus`);
-        // res.sendFile(filePath)
-    } catch(e) {
-        console.log(e);
-        res.sendStatus(404);
-    }
-
-})
-app.get('/api/sounds', async (req, res) => {
-    //let guild = req.query.server; // one day when sounds are tied to servers bot will need to get server id from query param
-    let soundData = await Sound.find({});
-    res.send(soundData);
-})
-app.post('/api/channels', async (req, res) => {
-    let channels = client.guilds.cache.get(req.body.guildId).channels.cache
-    let guild = await client.guilds.fetch(req.body.guildId);
-    let user = await guild.members.fetch(req.body.userId);
-
-    let result = [];
-    channels.forEach(c => {
-        if(c.type === "GUILD_VOICE" && user.permissionsIn(c).has("CONNECT")) {
-            result.push({id: c.id, name: c.name})
-        }
-    })
-    res.send(result);
-})
-
-app.post('/api/add', async (req, res) => {
-    let addResponse = await commands.add(req.body.args, null, req.body.username);
-    res.status(addResponse[0]).send(addResponse[1]);
-})
-
-app.post('/api/setFavorite', async (req, res) => {
-    let soundId = req.body?.soundId;
-    if (!soundId) {
-        return res.status(401).send('Invalid request format, unable to set favorite');
-    }
-    let userId = JSON.parse(req.headers.authorization).id;
-    let user = await User.findOne({ userId: userId});
-    if (!user) {
-        return res.status(401).send('Can\'t find user to update, try again later');
-    }
-    let favoriteIndex = user.favorites.indexOf(soundId);
-    user.updatedAt = Date.now();
-    if (favoriteIndex === -1) {
-        user.favorites.push(soundId);
-    } else {
-        user.favorites.splice(favoriteIndex, 1);
-    }
-    user.save().then(() => {
-        return res.status(200).send({favorites: user.favorites});
-    }).catch(e => {
-        return res.status(500).send('Error saving favorites, try again later');
-    });
-})
-
-async function validateUser(req) {
-    console.log("auth", req.headers.authorization);
-    let auth = JSON.parse(req.headers.authorization);
-    let user = await User.findOne({
-        userId: auth.id, 
-        access_token: auth.access_token
-    });
-    return !!user;
-}
-
-app.listen(PORT, () => {
-    console.log(`Now listening to requests on port ${PORT}`);
-});
+app(client);
